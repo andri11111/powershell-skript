147f17ba9a0930f8f98ca19a66f57cef78240599
Andri Tilsen <andri.tilsen@voco.ee>
2026-01-14 12:46:44 +0200

    ylesanded5 6

---
+++ practice5.ps1
@@ -0,0 +1,11 @@
+﻿Get-Process -Name notepad | Select-Object Name, Id
+
+Get-ChildItem C:\Temp\Test
+
+$file = Get-ChildItem -Path "C:\Temp\Test" -Filter *.csv | Select Name, Length
+$sizeinKB = $file.Length/1KB
+$sizeinMB = $file.Length/1MB
+Write-Host " "
+Write-Host "`nFileName : "$file.Name
+Write-Host "Size in KB : "$sizeinKB
+Write-Host "Size in MB : "$sizeinMB
---
+++ practice6.ps1
@@ -0,0 +1,19 @@
+﻿$csv = Import-Csv C:\Users\andri\Desktop\test123.csv
+$result = @()
+foreach($c in $csv)
+{
+if([int]$c.Age -ge 4 -and [int]$c.Age -le 10)
+{
+$school = "Junior"
+}
+elseif([int]$c.Age -ge 11 -and [int]$c.Age -le 17)
+{
+$school = "Senior"
+}
+$temp = [PSCustomObject]@{
+Name = $c.Name
+School = $school
+}
+$result += $temp
+}
+$result

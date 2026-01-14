fe53eb33ba6248fbd6cfe4a65319824ee393f20d
Andri Tilsen <andri.tilsen@voco.ee>
2026-01-14 08:59:46 +0200

    practice2

---
+++ practice2.ps1
@@ -0,0 +1,17 @@
+﻿#Practice 2
+$daysworked = @{
+    "John" = 12
+    "Joe" = 10
+    "Mary" = 18
+}
+$salaryperday = @{
+    "John" = 100
+    "Joe" = 120
+    "Mary" = 150
+}
+$salary = @{}
+foreach ($name in $daysWorked.Keys) {
+    $salary[$name] = $daysWorked[$name] * $salaryPerDay[$name]
+}
+
+$salary

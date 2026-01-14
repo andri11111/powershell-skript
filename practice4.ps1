b2ec874a4e7dc35cf0208a1948537c1bda73e96f
Andri Tilsen <andri.tilsen@voco.ee>
2026-01-14 09:14:18 +0200

    prac4

---
+++ practice4.ps1
@@ -0,0 +1,14 @@
+﻿[int]$value1 = Read-Host "Anna üks number: "
+[int]$value2 = Read-Host "Anna teine number: "
+if ($value1 -gt $value2)
+  {
+  Write-Host "Esimene number $value1 on suurem kui $value2"
+  }
+elseif ($value1 -lt $value2)
+  {
+  Write-Host "Teine number $value2 on suurem kui $value1"
+  }
+else
+  {
+  Write-Host "Numbrid on võrdsed"
+  }
